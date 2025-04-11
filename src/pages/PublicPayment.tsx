e99f85e1e48006f12cd8ac7911a666e9f4a3184a
Mohit Sharma <mohi@mediahubster.com>
2025-04-11 09:23:24 +0530

    working on the dev

--- src/pages/PublicPayment.tsx
+++ src/pages/PublicPayment.tsx
@@ -55,9 +55,12 @@ export default function PublicPayment() {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-indigo-600"></div>
+      <div>
+        {document.location.toString()}
       </div>
+      // <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      //   <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-indigo-600"></div>
+      // </div>
     );
   }
 
